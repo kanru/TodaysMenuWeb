a94da65e9926b2e2474661a4911ca5011010fb5d
Kan-Ru Chen <kanru@kanru.info>
2022-04-16 16:39:07 +0900

    build: upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import graphql from '@rollup/plugin-graphql'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), graphql()]
+    plugins: [react(), graphql()],
 })
